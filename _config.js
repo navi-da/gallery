f93479263612a60f51532b58ce6a855c04214a9c
navi-da <ivan.bunde@student.moringaschool.com>
2024-07-01 23:09:25 +0300

    Modify _config.js credentials

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://navi-da:<password>@moringadevops.md3ygz1.mongodb.net/?retryWrites=true&w=majority&appName=MoringaDevOps',
+    development: 'mongodb+srv://navi-da:<password>@moringadevops.md3ygz1.mongodb.net/?retryWrites=true&w=majority&appName=MoringaDevOps',
+    test: 'mongodb+srv://navi-da:<password>@moringadevops.md3ygz1.mongodb.net/?retryWrites=true&w=majority&appName=MoringaDevOps',
 }
 module.exports = config;
