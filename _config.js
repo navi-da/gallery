0418d991c8ba2c960fdfc87dc622c920c74a1d03
navi-da <ivan.bunde@student.moringaschool.com>
2024-07-02 00:12:34 +0300

    Update mongo password

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://navi-da:<password>@moringadevops.md3ygz1.mongodb.net/?retryWrites=true&w=majority&appName=MoringaDevOps',
-    development: 'mongodb+srv://navi-da:<password>@moringadevops.md3ygz1.mongodb.net/?retryWrites=true&w=majority&appName=MoringaDevOps',
-    test: 'mongodb+srv://navi-da:<password>@moringadevops.md3ygz1.mongodb.net/?retryWrites=true&w=majority&appName=MoringaDevOps',
+    production: 'mongodb+srv://navi-da:fr3QbfuiHX93vBwv@moringadevops.md3ygz1.mongodb.net/?retryWrites=true&w=majority&appName=MoringaDevOps',
+    development: 'mongodb+srv://navi-da:fr3QbfuiHX93vBwv@moringadevops.md3ygz1.mongodb.net/?retryWrites=true&w=majority&appName=MoringaDevOps',
+    test: 'mongodb+srv://navi-da:fr3QbfuiHX93vBwv@moringadevops.md3ygz1.mongodb.net/?retryWrites=true&w=majority&appName=MoringaDevOps',
 }
 module.exports = config;
